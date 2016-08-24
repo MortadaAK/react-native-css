01fa4a8c0da344def062d37aadd46c84f545d9e6
MortadaAK <mortadaajk@gmail.com>
2016-08-25 02:50:37 +0300

    Update index.js

--- build/index.js
+++ build/index.js
@@ -181,7 +181,7 @@ var ReactNativeCss = (function () {
                   if (_utilsJs2['default'].arrayContains(property, numberize)) {
                     var reg = /^('|")JS\((.*)\)\1$/;
                     if(reg.test(value)){
-                      styles[_toCamelCase(property)] = value.match(reg)[2]
+                      styles[_toCamelCase(property)] = "JS("+value.match(reg)[2]+")"
                     } else {
                       var value = value.replace(/px|\s*/g, '');
                       styles[_toCamelCase(property)] = parseFloat(value);
